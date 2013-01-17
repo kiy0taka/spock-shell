75975e40f0b437585293348d1894b2292157f892
kiy0taka <kiy0taka333@gmail.com>
2013-01-17 23:56:07 +0900

    added license.

--- src/main/groovy/org/kiy0taka/spock/shell/ShellSpec.groovy
+++ src/main/groovy/org/kiy0taka/spock/shell/ShellSpec.groovy
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2012-2013 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.kiy0taka.spock.shell
 
 import org.junit.Rule
@@ -6,6 +21,12 @@ import org.junit.rules.TestName
 import spock.lang.Shared
 import spock.lang.Specification
 
+/**
+ * Spock extension for shell script.
+ *
+ * @author Kiyotaka Oku
+ * @author Tsuyoshi Yamamoto
+ */
 class ShellSpec extends Specification {
 
     @Rule
